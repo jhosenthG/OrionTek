d7e99d4461d04027f49b034f720b5c2e9c1083e0
jhosenthg <jhosenth@gmail.com>
2026-04-23 12:26:48 -0400

    (Add) use cases for retrieving client details and list of clients

--- app/src/main/java/com/jhosenthg/oriontek/domain/useCase/GetClientDetailsUseCase.kt
+++ app/src/main/java/com/jhosenthg/oriontek/domain/useCase/GetClientDetailsUseCase.kt
@@ -1,2 +1,14 @@
 package com.jhosenthg.oriontek.domain.useCase
 
+import com.jhosenthg.oriontek.domain.entities.Client
+import com.jhosenthg.oriontek.domain.repository.ClientRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetClientDetailsUseCase @Inject constructor(
+    private val repository: ClientRepository
+) {
+    operator fun invoke(clientId: String): Flow<Result<Client>> {
+        return repository.getClientById(clientId)
+    }
+}
--- app/src/main/java/com/jhosenthg/oriontek/domain/useCase/GetClientsUseCase.kt
+++ app/src/main/java/com/jhosenthg/oriontek/domain/useCase/GetClientsUseCase.kt
@@ -1,2 +1,14 @@
 package com.jhosenthg.oriontek.domain.useCase
 
+import com.jhosenthg.oriontek.domain.entities.Client
+import com.jhosenthg.oriontek.domain.repository.ClientRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetClientsUseCase @Inject constructor(
+    private val repository: ClientRepository
+) {
+    operator fun invoke(): Flow<Result<List<Client>>> {
+        return repository.getClients()
+    }
+}
