7065f26575112aada2113eefc66a60ac68ce7ca2
jhosenthg <jhosenth@gmail.com>
2026-04-23 15:28:53 -0400

    (Add) implement OrionTekApi interface for client data retrieval and create OrionTekApp class for application setup

---
+++ app/src/main/java/com/jhosenthg/oriontek/OrionTekApp.kt
@@ -0,0 +1,5 @@
+package com.jhosenthg.oriontek
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+@HiltAndroidApp
+class OrionTekApp : Application()
--- app/src/main/java/com/jhosenthg/oriontek/data/remote/OrionTekApi.kt
+++ app/src/main/java/com/jhosenthg/oriontek/data/remote/OrionTekApi.kt
@@ -1,4 +1,15 @@
 package com.jhosenthg.oriontek.data.remote
 
-class OrionTekApi {
+import com.jhosenthg.oriontek.data.remote.dto.ClientDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface OrionTekApi {
+	@GET("clients")
+	suspend fun getClients(): List<ClientDto>
+
+	@GET("clients/{clientId}")
+	suspend fun getClientById(
+		@Path("clientId") clientId: String
+	): ClientDto
 }
